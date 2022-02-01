df4a73e1a04dc101e330d34a2f7a472a9dd2ca35
Hritvik Semwal <hritvik.semwal2018@vitbhopal.ac.in>
2022-02-01 12:07:04 +0530

    updated sync read with error handling

--- JavaScriptPrograms/Day2/synchrounousFileRead.js
+++ JavaScriptPrograms/Day2/synchrounousFileRead.js
@@ -7,3 +7,19 @@ console.log(data)
 fs.writeFileSync("./sample.txt","new content")
 const data2=fs.readFileSync("./sample.txt",'utf-8')
 console.log(data2)
+
+//File Reading with Error Handling
+
+// const fs=require("fs")
+// fs.readFile('./data/sample.txt','utf-8',(err,data)=>{
+// if(err){
+//     console.log(err)
+// }
+// setTimeout(()=>console.log(data),2000)
+// })
+// fs.readFile('./data/sample2.txt','utf-8',(err,data)=>{
+//     if(err){
+//         console.log(err)
+//     }
+//     console.log(data)
+//     })
